e3578eeb4fde1d808650d9f7aec1920ae24537c9
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2019-12-01 18:56:12 +0300

    Initial commit - day 1

---
+++ 01.ts
@@ -0,0 +1,16 @@
+import * as fs from 'fs';
+const input = fs.readFileSync('data/01.txt', { encoding: 'utf-8' });
+const lines = input.split('\n').filter((line) => line);
+let s = 0;
+let s2 = 0;
+for (const line of lines) {
+    let mass = parseInt(line);
+    mass = Math.floor(mass / 3) - 2;
+    s += mass;
+    while (mass > 0) {
+        s2 += mass;
+        mass = Math.floor(mass / 3) - 2;
+    }
+}
+console.log('part 1:', s);
+console.log('part 2:', s2);
