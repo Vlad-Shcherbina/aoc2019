0f882ec79dd81659f0de08fd5885b98147d0c944
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2019-12-08 02:37:37 +0300

    Day 7 part 2

--- 07.ts
+++ 07.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs'
 import * as intcode from './intcode.js'
+import * as assert from 'assert'
 
 let prog = fs.readFileSync('data/07.txt', {encoding: 'utf8'})
     .split(',')
@@ -28,3 +29,55 @@ function rec(cnt: number, value: number) {
 
 rec(0, 0)
 console.log('part 1', best)
+
+function runLoop(modes: number[]): number {
+    let ms = modes.map((mode) => {
+        let m = new intcode.Machine(prog)
+        m.feedInput([mode])
+        return m
+    })
+    let input = 0
+    while (true) {
+        for (let m of ms) {
+            m.feedInput([input])
+            let res = m.run()
+            if (res.event == 'OUTPUT') {
+                input = res.output
+            } else {
+                assert.fail(`${res}`)
+            }
+        }
+        let res = ms[ms.length - 1].run()
+        if (res.event == 'HALT') {
+            return input
+        } else if (res.event == 'INPUT') {
+            continue
+        } else if (res.event == 'OUTPUT') {
+            assert.fail(`${res}`)
+        } else {
+            let _: never = res
+        }
+    }
+}
+
+let modes: number[] = []
+best = -1e10
+
+function rec2() {
+    if (modes.length == 5) {
+        let value = runLoop(modes)
+        best = Math.max(best, value)
+        return
+    }
+    for (let i = 5; i <= 9; i++) {
+        if (modes.includes(i)) {
+            continue
+        }
+        modes.push(i)
+        rec2()
+        modes.pop()
+    }
+}
+
+rec2()
+console.log('part 2', best)
--- intcode.ts
+++ intcode.ts
@@ -1,87 +1,119 @@
 import * as assert from 'assert'
 
-function readOperand(ip: number, pos: number, mem: number[]): number {
-    let mode;
-    if (pos === 1) {
-        mode = Math.floor(mem[ip] / 100) % 10
-    } else if (pos === 2) {
-        mode = Math.floor(mem[ip] / 1000) % 10
-    } else if (pos === 3) {
-        mode = Math.floor(mem[ip] / 10000) % 10
-    } else {
-        throw pos
-    }
-    if (mode === 0) {
-        return mem[mem[ip + pos]]
-    } else if (mode === 1) {
-        return mem[ip + pos]
-    } else {
-        throw mode
-    }
-}
+export class Machine {
+    mem: number[]
+    ip: number
+    input: number[]
 
-export function run(mem: number[], inputs: number[]): number[] {
-    let outputs: number[] = []
-    let input_pos = 0
-    let ip = 0
-    while (true) {
-        assert.ok(0 <= ip && ip < mem.length)
-        let modes = Math.floor(mem[ip] / 100)
-        let opcode = mem[ip] % 100
-        if (opcode === 1) {
-            assert.strictEqual(Math.floor(mem[ip] / 10000), 0)
-            mem[mem[ip + 3]] = readOperand(ip, 1, mem) + readOperand(ip, 2, mem)
-            ip += 4
-        } else if (opcode == 2) {
-            assert.strictEqual(Math.floor(mem[ip] / 10000), 0)
-            mem[mem[ip + 3]] = readOperand(ip, 1, mem) * readOperand(ip, 2, mem)
-            ip += 4
-        } else if (opcode == 3) {
-            assert.strictEqual(Math.floor(mem[ip] / 100), 0)
-            assert.ok(input_pos < inputs.length)
-            // console.log('read 5')
-            mem[mem[ip + 1]] = inputs[input_pos++]
-            ip += 2
-        } else if (opcode == 4) {
-            // console.log('write', readOperand(ip, 1, mem))
-            outputs.push(readOperand(ip, 1, mem))
-            ip += 2
-        } else if (opcode == 5) {
-            if (readOperand(ip, 1, mem) !== 0) {
-                ip = readOperand(ip, 2, mem)
+    constructor(prog: number[]) {
+        this.mem = prog.slice(0)
+        this.ip = 0
+        this.input = []
+    }
+
+    feedInput(input: number[]) {
+        this.input = this.input.concat(input)
+    }
+
+    run(): { event: 'INPUT' } | { event: 'OUTPUT', output: number } | { event: 'HALT' } {
+        let mem = this.mem
+        while (true) {
+            assert.ok(0 <= this.ip && this.ip < mem.length)
+            let modes = Math.floor(mem[this.ip] / 100)
+            let opcode = mem[this.ip] % 100
+            if (opcode === 1) {
+                assert.strictEqual(Math.floor(mem[this.ip] / 10000), 0)
+                mem[mem[this.ip + 3]] = this.readOperand(1) + this.readOperand(2)
+                this.ip += 4
+            } else if (opcode == 2) {
+                assert.strictEqual(Math.floor(mem[this.ip] / 10000), 0)
+                mem[mem[this.ip + 3]] = this.readOperand(1) * this.readOperand(2)
+                this.ip += 4
+            } else if (opcode == 3) {
+                assert.strictEqual(Math.floor(mem[this.ip] / 100), 0)
+                if (this.input.length == 0) {
+                    return { event: 'INPUT' }
+                }
+                mem[mem[this.ip + 1]] = this.input[0]
+                this.input = this.input.slice(1)
+                this.ip += 2
+            } else if (opcode == 4) {
+                let output = this.readOperand(1)
+                this.ip += 2
+                return { event: 'OUTPUT', output: output }
+            } else if (opcode == 5) {
+                if (this.readOperand(1) !== 0) {
+                    this.ip = this.readOperand(2)
+                } else {
+                    this.ip += 3
+                }
+            } else if (opcode == 6) {
+                if (this.readOperand(1) === 0) {
+                    this.ip = this.readOperand(2)
+                } else {
+                    this.ip += 3
+                }
+            } else if (opcode == 7) {
+                assert.strictEqual(Math.floor(mem[this.ip] / 10000), 0)
+                if (this.readOperand(1) <
+                    this.readOperand(2)) {
+                    mem[mem[this.ip + 3]] = 1;
+                } else {
+                    mem[mem[this.ip + 3]] = 0;
+                }
+                this.ip += 4
+            } else if (opcode == 8) {
+                assert.strictEqual(Math.floor(mem[this.ip] / 10000), 0)
+                if (this.readOperand(1) ===
+                    this.readOperand(2)) {
+                    mem[mem[this.ip + 3]] = 1;
+                } else {
+                    mem[mem[this.ip + 3]] = 0;
+                }
+                this.ip += 4
+            } else if (opcode == 99) {
+                return { event: 'HALT' }
             } else {
-                ip += 3
+                assert.fail(`unknown opcode ${opcode}`)
             }
-        } else if (opcode == 6) {
-            if (readOperand(ip, 1, mem) === 0) {
-                ip = readOperand(ip, 2, mem)
-            } else {
-                ip += 3
-            }
-        } else if (opcode == 7) {
-            assert.strictEqual(Math.floor(mem[ip] / 10000), 0)
-            if (readOperand(ip, 1, mem) <
-                readOperand(ip, 2, mem)) {
-                mem[mem[ip + 3]] = 1;
-            } else {
-                mem[mem[ip + 3]] = 0;
-            }
-            ip += 4
-        } else if (opcode == 8) {
-            assert.strictEqual(Math.floor(mem[ip] / 10000), 0)
-            if (readOperand(ip, 1, mem) ===
-                readOperand(ip, 2, mem)) {
-                mem[mem[ip + 3]] = 1;
-            } else {
-                mem[mem[ip + 3]] = 0;
-            }
-            ip += 4
-        } else if (opcode == 99) {
-            break
-        } else {
-            assert.fail(`unknown opcode ${opcode}`)
         }
     }
 
-    return outputs
+    readOperand(pos: number) {
+        let mode;
+        if (pos === 1) {
+            mode = Math.floor(this.mem[this.ip] / 100) % 10
+        } else if (pos === 2) {
+            mode = Math.floor(this.mem[this.ip] / 1000) % 10
+        } else if (pos === 3) {
+            mode = Math.floor(this.mem[this.ip] / 10000) % 10
+        } else {
+            throw pos
+        }
+        if (mode === 0) {
+            return this.mem[this.mem[this.ip + pos]]
+        } else if (mode === 1) {
+            return this.mem[this.ip + pos]
+        } else {
+            throw mode
+        }
+    }
+}
+
+export function run(prog: number[], inputs: number[]): number[] {
+    let m = new Machine(prog)
+    m.feedInput(inputs)
+    let result = []
+    while (true) {
+        let res = m.run()
+        if (res.event == 'INPUT') {
+            assert.fail('not enough input')
+        } else if (res.event == 'OUTPUT') {
+            result.push(res.output)
+        } else if (res.event = 'HALT') {
+            return result
+        } else {
+            let _: never = res
+        }
+    }
 }
