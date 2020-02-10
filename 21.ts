2eff216747b058a1b9cbb04168981f0280b90366
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2020-02-10 14:00:42 +0300

    Day 21 part 2

--- 21.ts
+++ 21.ts
@@ -2,8 +2,40 @@ import * as fs from 'fs'
 import * as intcode from './intcode.js'
 import { assert } from './util.js'
 
+function run_ascii(prog: bigint[], input: string): number | null {
+    let m = new intcode.Machine(prog)
+    for (let ch of input) {
+        m.feedInput([BigInt(ch.charCodeAt(0))])
+    }
+    // TODO: deduplicate (anchor: jbWYTVqwDjaN)
+    let outputs = ''
+    while (true) {
+        let res = m.run()
+        if (res.event === 'HALT') {
+            console.log('HALT')
+            return null
+        } else if (res.event === 'OUTPUT') {
+            if (res.output > 127n) {
+                return Number(res.output)
+            } else if (res.output === 10n) {
+                console.log(outputs)
+                outputs = ''
+            } else {
+                outputs += String.fromCharCode(Number(res.output))
+            }
+        } else if (res.event === 'INPUT') {
+            console.log('INPUT')
+            assert(false)
+        } else {
+            let _: never = res
+            assert(false)
+        }
+    }
+}
+
 let prog = fs.readFileSync('data/21.txt', {encoding: 'utf8'}).split(',').map(BigInt)
 
+// J = not (A and B and C) and D
 let program = `\
 NOT J T
 OR T J
@@ -14,31 +46,21 @@ NOT J J
 AND D J
 WALK
 `
-let m = new intcode.Machine(prog)
-for (let ch of program) {
-    m.feedInput([BigInt(ch.charCodeAt(0))])
-}
-// TODO: deduplicate (anchor: jbWYTVqwDjaN)
-let outputs = ''
-while (true) {
-    let res = m.run()
-    if (res.event === 'HALT') {
-        console.log('HALT')
-        break
-    } else if (res.event === 'OUTPUT') {
-        if (res.output > 127n) {
-            console.log('part 1:', Number(res.output))
-        } else if (res.output === 10n) {
-            console.log(outputs)
-            outputs = ''
-        } else {
-            outputs += String.fromCharCode(Number(res.output))
-        }
-    } else if (res.event === 'INPUT') {
-        console.log('INPUT')
-        break
-    } else {
-        let _: never = res
-        assert(false)
-    }
-}
+console.log('part 1', run_ascii(prog, program))
+
+// J = not (A and B and C) and D and (E or H)
+program = `\
+NOT J T
+OR T J
+AND A J
+AND B J
+AND C J
+NOT J J
+AND D J
+NOT E T
+NOT T T
+OR H T
+AND T J
+RUN
+`
+console.log('part 2', run_ascii(prog, program))
