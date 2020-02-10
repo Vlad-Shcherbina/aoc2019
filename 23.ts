200b6fc2145fe52ebf0652f6dc7a18961858e442
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2020-02-10 17:30:42 +0300

    Day 23 part 2

--- 23.ts
+++ 23.ts
@@ -6,6 +6,7 @@ interface State {
     m: intcode.Machine
     queue: bigint[]
     output: bigint[]
+    waiting: number
 }
 
 let prog = fs.readFileSync('data/23.txt', {encoding: 'utf8'}).split(',').map(BigInt)
@@ -13,10 +14,13 @@ let prog = fs.readFileSync('data/23.txt', {encoding: 'utf8'}).split(',').map(Big
 let states: State[] = Array.from(new Array(50).keys()).map(i => {
     let m = new intcode.Machine(prog)
     m.feedInput([BigInt(i)])
-    return { m, queue: [], output: [] }
+    return { m, queue: [], output: [], waiting: 0 }
 })
 
-outer:
+let prev_nat_y: bigint | null = null
+let nat: bigint[] | null = null
+
+console.time('part 1')
 while (true) {
     for (let s of states) {
         let res = s.m.run(1)
@@ -27,8 +31,10 @@ while (true) {
                 let x = s.queue.slice(0, 2)
                 s.queue = s.queue.slice(2)
                 s.m.feedInput(x)
+                s.waiting = 0
             } else {
                 s.m.feedInput([-1n])
+                s.waiting++
             }
             res = s.m.run(1)
             assert(res.event !== 'INPUT')
@@ -40,15 +46,34 @@ while (true) {
                 s.output.push(res.output)
                 if (s.output.length === 3) {
                     if (s.output[0] === 255n) {
-                        console.log('part 1', Number(s.output[2]))
-                        break outer
+                        if (nat === null) {
+                            console.timeEnd('part 1')
+                            console.log('part 1', Number(s.output[2]))
+                            console.time('part 2')
+                        }
+                        nat = s.output.slice(1)
+                        s.output = []
+                        s.waiting = 0
+                    } else {
+                        let recepient = states[Number(s.output[0])]
+                        recepient.queue.push(...s.output.slice(1))
+                        s.output = []
+                        s.waiting = 0
                     }
-                    let recepient = states[Number(s.output[0])]
-                    recepient.queue.push(...s.output.slice(1))
-                    s.output = []
                 }
                 break
             }
         }
+    }
+    if (states.every(s => s.waiting >= 2 && s.queue.length === 0)) {
+        assert(nat !== null)
+        if (nat[1] === prev_nat_y) {
+            console.log('part 2', Number(prev_nat_y))
+            console.timeEnd('part 2')
+            break
+        }
+        prev_nat_y = nat[1]
+        let recepient = states[0]
+        recepient.queue.push(...nat)
     }
 }
